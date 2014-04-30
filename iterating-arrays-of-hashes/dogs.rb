528f2c864c6a356494d74f4f1bd87c760949be47
Ellie Schneiders <ems2141@gmail.com>
2014-04-30 09:33:56 -0600

    first 5 tests pass.. saving the last two for warm-up tomorrow

--- iterating-arrays-of-hashes/dogs.rb
+++ iterating-arrays-of-hashes/dogs.rb
@@ -1,20 +1,20 @@
 class Dogs
-  POOR      = (0..5).to_a.sample
-  AVERAGE   = (6..10).to_a.sample
+  POOR = (0..5).to_a.sample
+  AVERAGE = (6..10).to_a.sample
   EXCELLENT = (11..15).to_a.sample
 
   def initialize
-    joe   = {
-      :name => {:first => "Joe", :last=> "Smith"},
-      :owner_quality => EXCELLENT
+    joe = {
+        :name => {:first => "Joe", :last => "Smith"},
+        :owner_quality => EXCELLENT
     }
     sarah = {
-      :name => {:first => "Sarah", :last => "Smith"},
-      :owner_quality => AVERAGE
+        :name => {:first => "Sarah", :last => "Smith"},
+        :owner_quality => AVERAGE
     }
     andrew = {
-      :name => {:first => "Andrew", :last => "Beter"},
-      :owner_quality => AVERAGE
+        :name => {:first => "Andrew", :last => "Beter"},
+        :owner_quality => AVERAGE
     }
 
     @dogs = [{:name => "Fido", :size => :large, :owner => joe},
@@ -29,27 +29,70 @@ class Dogs
   # only edit below this line
 
   def small_dogs
-    # only use the @dogs instance variable
+    small_dogs =[]
+    @dogs.each do |dog|
+      if dog[:size] == :small
+        small_dogs << dog
+      end
+    end
+    small_dogs
   end
 
   def huge_dog
-    # only use the @dogs instance variable
+    huge_dog =[]
+    @dogs.each do |dog|
+      if dog[:size] == :huge
+        huge_dog << dog
+      end
+    end
+    huge_dog.first
   end
 
   def large_dog_names
-    # only use the @dogs instance variable
+    large_dogs =[]
+    large_dogs_names =[]
+
+    @dogs.each do |dog|
+      if dog[:size] == :large
+        large_dogs << dog
+      end
+    end
+
+    large_dogs.each do |name|
+      large_dogs_names << name[:name]
+    end
+    large_dogs_names
+
   end
 
   def joes_large_dogs
-    # only use the @dogs instance variable
+    large_dogs =[]
+
+    @dogs.each do |dog|
+      if dog[:size] == :large && dog[:owner][:name][:first] == "Joe"
+        large_dogs << dog[:name]
+      end
+    end
+
+    large_dogs
   end
 
   def sizes
-    # only use the @dogs instance variable
+    dog_sizes = []
+    @dogs.each do |dog|
+      if !dog_sizes.include?(dog[:size])
+        dog_sizes << dog[:size]
+      end
+    end
+    dog_sizes
   end
 
   def owners
-    # only use the @dogs instance variable
+    dog_owners =[]
+    @dogs.each do |dog|
+
+    end
+    dog_owners
   end
 
   def average_owners
